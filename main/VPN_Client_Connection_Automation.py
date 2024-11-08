799cc30a30b8a1b1652a042ae3d41de56eb49e41
Baptiste MONDION <bmondion@hotmail.com>
2024-11-08 11:30:30 +0100

    Update VPN_Client_Connection_Automation.py

--- main/VPN_Client_Connection_Automation.py
+++ main/VPN_Client_Connection_Automation.py
@@ -1,1 +1,14 @@
+import os
+import subprocess
 
+# Vérifier la connexion VPN
+def check_vpn_connection():
+    result = subprocess.run(['curl', 'https://api.myip.com'], stdout=subprocess.PIPE)
+    ip_info = result.stdout.decode('utf-8')
+    
+    if '10.8.0.' in ip_info:
+        print("Connexion VPN réussie")
+    else:
+        print("Échec de la connexion VPN")
+
+check_vpn_connection()
